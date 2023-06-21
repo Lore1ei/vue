373227f557e8218d97a3a05516ca4d7e902139bb
Lore1ei <harubator@gmail.com>
2023-06-21 20:57:12 +0700

    scoped css

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="post">
+      <div><strong>Название:</strong> Пост о JavaScript</div>
+      <div><strong>Описание:</strong> JavaScript язык программировани</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        likes: 0,
+        dislikes: 0,
+      }
+    },
+    methods: {
+      addLike(){
+        this.likes += 1;
+      },
+      addDislike(){
+        this.dislikes +=1;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>

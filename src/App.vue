2bda77a47a68fc92a689af9f06a294ebd7636b4d
Lore1ei <harubator@gmail.com>
2023-06-22 12:48:33 +0700

    working with form

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
-    <div class="post">
-      <div><strong>Название:</strong> Пост о JavaScript</div>
-      <div><strong>Описание:</strong> JavaScript язык программировани</div>
+  <div class="app">
+      <form>
+        <h4>Создание поста</h4>
+        <input v-bind:value="title"
+               @input="titleValue"
+               class="input"
+               type="text"
+               placeholder="Название поста">
+        <input v-bind:value="body" class="input" type="text" placeholder="Описание поста">
+        <button class="btn" type="submit" @click="createPost">Создать</button>
+      </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong> {{post.title}}</div>
+      <div><strong>Описание:</strong> {{post.body}}</div>
     </div>
   </div>
 </template>
@@ -11,21 +21,55 @@
   export default {
     data(){
       return{
-        likes: 0,
-        dislikes: 0,
+        posts: [
+          {id: 1, title: 'Javascript 1', body: 'Описание поста 1'},
+          {id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
+          {id: 3, title: 'Javascript 3', body: 'Описание поста 3'},
+          {id: 4, title: 'Javascript 4', body: 'Описание поста 4'},
+        ],
+        title: '',
+        body: '',
       }
     },
     methods: {
-      addLike(){
-        this.likes += 1;
-      },
-      addDislike(){
-        this.dislikes +=1;
+      createPost(){
+        console.log()
       }
     }
   }
 </script>
 
 <style>
-
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  .app{
+    padding: 20px;
+  }
+  form{
+    display: flex;
+    flex-direction: column;
+  }
+  .post{
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+  }
+  .input{
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+  }
+  .btn{
+    align-self: flex-end;
+    margin-top: 15px;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+    cursor: pointer;
+  }
 </style>
